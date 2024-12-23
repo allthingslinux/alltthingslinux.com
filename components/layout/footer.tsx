eac21e4d5c62055a58cb0956a68323a37983b7b9
kzndotsh <admin@kaizen.wtf>
2024-12-23 00:24:12 -0500

    feat(consent.jsx): add consent components for privacy, cookies, and terms to manage legal compliance feat(footer.tsx): integrate consent components into footer for improved user access to legal information

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { BsOpencollective } from 'react-icons/bs';
 import { FaDiscord, FaGithub } from 'react-icons/fa';
 
+import { Privacy, Cookies, Terms } from '@/components/consent';
+
 interface NavigationItem {
   name: string;
   href: string;
@@ -48,34 +50,40 @@ const navigation: Navigation = {
 
 export default function Footer() {
   return (
-    <footer>
-      <div className="border-t border-slate-800 max-w-5xl mx-auto px-6 lg:px-8"></div>
-
-      <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
+    <footer className="bg-gray-900 py-8">
+      <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
         <nav
           aria-label="Footer"
-          className="-mb-6 flex flex-wrap justify-center gap-x-12 gap-y-3 text-sm/6">
+          className="flex flex-wrap justify-center gap-x-12 gap-y-3 text-center text-base"
+        >
           {navigation.main.map((item) => (
             <a
               key={item.name}
               href={item.href}
-              className="text-gray-400 hover:text-white">
+              className="text-gray-400 hover:text-white transition-colors"
+            >
               {item.name}
             </a>
           ))}
         </nav>
-        <div className="mt-16 flex justify-center gap-x-10">
+        <div className="mt-8 flex justify-center gap-x-10">
           {navigation.social.map((item) => (
             <a
               key={item.name}
               href={item.href}
-              className="text-gray-400 hover:text-gray-300">
+              className="text-gray-400 hover:text-gray-300 transition-colors"
+            >
               <span className="sr-only">{item.name}</span>
               <item.icon aria-hidden="true" className="h-6 w-6" />
             </a>
           ))}
         </div>
-        <p className="mt-10 text-center text-sm/6 text-gray-400">
+        <div className="mt-8 flex justify-center gap-x-4 text-gray-400 hover:text-gray-300 text-sm text-center flex-wrap gap-y-4">
+          <Privacy />
+          <Cookies />
+          <Terms />
+        </div>
+        <p className="mt-6 text-center text-sm text-gray-400 text-balance">
           &copy; 2024 All Things Linux • Made with ❤️ • All Rights Reserved.
         </p>
       </div>
